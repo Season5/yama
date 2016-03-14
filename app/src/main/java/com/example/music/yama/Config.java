f2d3925591494482152d9e46a9de55470660c7eb
martin <martow17@gmail.com>
2016-03-14 10:54:36 +0300

    add yama

---
+++ app/src/main/java/com/example/music/yama/Config.java
@@ -0,0 +1,18 @@
+package com.example.music.yama;
+
+/**
+ * Created by martin on 3/13/16.
+ */
+public final class Config {
+
+    /**
+     * Created by echessa on 7/17/15.
+     */
+
+        private Config() {
+        }
+
+        public static final String YOUTUBE_API_KEY = " AIzaSyB_zoMaDCb9gypuy84C8NAQgtlvUgvrVk4 ";
+
+    }
+
